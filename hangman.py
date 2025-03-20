4d855d4ac42366bd3455cb4c6fa4ec12b9103711
Dan Aguam <danaguam@Sheilas-MacBook-Air.local>
2025-03-20 20:37:57 +0800

    My first commit.

---
+++ hangman.py
@@ -0,0 +1,47 @@
+import random
+
+list_of_words = ["Jigsaw","Oxygen","Zephyr","Quartz","Mystify","Horizon","Glacier","Cipher","Vortex","Buzzer"]
+word = random.choice(list_of_words)
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
+    letters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter: "
+        char = input(msg)
+        if char in letters:
+            cind = letters.index(char)
+            board[cind] = char
+            letters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n"
+              .join(stages[0: wrong]))
+        print("You lose! It was {}.".format(word))
+
+
+print(word)
+hangman(word.lower())
+
